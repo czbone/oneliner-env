e82ee97c1d11212250927ea4bbef5ffd8e2a1df2
naoki hirata <naoki@magic3.org>
2018-12-05 11:06:35 +0900

    スクリプト追加。

---
+++ script/build_lemp.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+declare OS="unsupported os"
+if [ "$(uname)" == 'Darwin' ]; then
+  OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  RELEASE_FILE=/etc/os-release
+  if grep '^NAME="CentOS' "${RELEASE_FILE}" >/dev/null; then
+   OS=CentOS
+  elif grep '^NAME="Amazon' "${RELEASE_FILE}" >/dev/null; then
+   OS="Amazon Linux"
+  elif grep '^NAME="Ubuntu' "${RELEASE_FILE}" >/dev/null; then
+   OS=Ubuntu
+  else
+    echo "Your platform is not supported."
+    uname -a
+    exit 1
+  fi
+elif [ "$(expr substr $(uname -s) 1 6)" == 'CYGWIN' ]; then
+  OS='Cygwin'
+else
+  echo "Your platform is not supported."
+  uname -a
+  exit 1
+fi
+

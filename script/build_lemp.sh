b2011f97cef0a4df554afa72966455ac3236faac
naoki hirata <naoki@magic3.org>
2018-12-05 14:54:18 +0900

    起動コマンドファイル更新。

--- script/build_lemp.sh
+++ script/build_lemp.sh
@@ -1,27 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
+# Check os version
 declare OS="unsupported os"
+
 if [ "$(uname)" == 'Darwin' ]; then
-  OS='Mac'
+    OS='Mac'
+    uname -a
+    exit 1
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  RELEASE_FILE=/etc/os-release
-  if grep '^NAME="CentOS' "${RELEASE_FILE}" >/dev/null; then
-   OS=CentOS
-  elif grep '^NAME="Amazon' "${RELEASE_FILE}" >/dev/null; then
-   OS="Amazon Linux"
-  elif grep '^NAME="Ubuntu' "${RELEASE_FILE}" >/dev/null; then
-   OS=Ubuntu
-  else
+    RELEASE_FILE=/etc/os-release
+    if grep '^NAME="CentOS' "${RELEASE_FILE}" >/dev/null; then
+        OS="CentOS"
+    elif grep '^NAME="Amazon' "${RELEASE_FILE}" >/dev/null; then
+        OS="Amazon Linux"
+        uname -a
+        exit 1
+    elif grep '^NAME="Ubuntu' "${RELEASE_FILE}" >/dev/null; then
+        OS="Ubuntu"
+    else
+        echo "Your platform is not supported."
+        uname -a
+        exit 1
+    fi
+elif [ "$(expr substr $(uname -s) 1 6)" == 'CYGWIN' ]; then
+    OS='Cygwin'
+    uname -a
+    exit 1
+else
     echo "Your platform is not supported."
     uname -a
     exit 1
-  fi
-elif [ "$(expr substr $(uname -s) 1 6)" == 'CYGWIN' ]; then
-  OS='Cygwin'
-else
-  echo "Your platform is not supported."
-  uname -a
-  exit 1
 fi
 
 echo "Get OS is $OS"
+echo "########################################################################"
+echo "# START BUILDING ENVIRONMENT"
+echo "########################################################################"
+if [ $OS == 'CentOS' ]; then
+    INSTALL_PACKAGE_CMD="yum -y install"
+elseif [ $OS == 'Ubuntu' ]; then
+    INSTALL_PACKAGE_CMD="apt-get install"
+fi
+$INSTALL_PACKAGE_CMD git
+$INSTALL_PACKAGE_CMD ansible
+

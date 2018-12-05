ad53504cf1b1590f865ce70ed288d57779160d8a
naoki hirata <naoki@magic3.org>
2018-12-05 22:10:46 +0900

    テストスクリプト更新。

--- test/test_build_lemp.sh
+++ test/test_build_lemp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/czbone/oneliner-env/master/script/build_lemp_centos7.sh | sh /dev/stdin -test
+curl https://raw.githubusercontent.com/czbone/oneliner-env/master/script/build_lemp.sh | sh /dev/stdin -test

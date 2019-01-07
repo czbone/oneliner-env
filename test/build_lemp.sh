b24810802ae84752a89fa9e6748e0a1f9d7bc0aa
naoki hirata <naoki@magic3.org>
2019-01-07 23:24:20 +0900

    テスト用起動スクリプト更新。

--- test/build_lemp.sh
+++ test/build_lemp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/czbone/oneliner-env/master/script/build_lemp.sh | bash /dev/stdin -test
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/czbone/oneliner-env/master/script/build_lemp.sh | bash /dev/stdin -test

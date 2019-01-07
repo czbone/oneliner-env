e9e51dbed6445e7dc61b02a4dea715dbf8d03a2e
naoki hirata <naoki@magic3.org>
2019-01-07 23:09:05 +0900

    起動スクリプト修正。

--- script/build_lemp.sh
+++ script/build_lemp.sh
@@ -69,7 +69,7 @@ echo "# START BUILDING ENVIRONMENT                                           #"
 echo "########################################################################"
 echo "Param=$1"
 # Get test mode
-if [ $1 = '-test' ]; then
+if [[ "$1" == '-test' ]]; then
     readonly TEST_MODE="true"
     
     echo "################# START TEST MODE #################"

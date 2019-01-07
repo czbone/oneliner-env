8318d84ac1a3cb88f6f10f5e04f78280bb013514
naoki hirata <naoki@magic3.org>
2019-01-08 06:18:33 +0900

    起動スクリプト更新。

--- test/build_lemp.sh
+++ test/build_lemp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/czbone/oneliner-env/master/script/build_lemp.sh | bash /dev/stdin -test
+curl https://raw.githubusercontent.com/czbone/oneliner-env/master/script/build_lemp.sh | sh /dev/stdin -test

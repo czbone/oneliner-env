2d7aaee30119c75cd5515244045fe99283a531d8
naoki hirata <naoki@magic3.org>
2019-06-04 05:59:00 +0900

    テスト用変更。

--- script/build_lemp.sh
+++ script/build_lemp.sh
@@ -84,7 +84,7 @@ if [ $OS == 'CentOS' ]; then
 elif [ $OS == 'Ubuntu' ]; then
     if ! type -P ansible >/dev/null ; then
         #INSTALL_PACKAGE_CMD="apt install"
-        INSTALL_PACKAGE_CMD="apt --purge --yes install"
+        INSTALL_PACKAGE_CMD="apt -y install"
     
         # Repository update for ansible
         apt install software-properties-common

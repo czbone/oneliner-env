f0d9863f6698bf61bb107363db445a43073b8d21
naoki hirata <naoki@magic3.org>
2019-08-27 16:11:08 +0900

    Ubuntu用修正。

--- script/build_lemp_ffmpeg.sh
+++ script/build_lemp_ffmpeg.sh
@@ -87,11 +87,11 @@ elif [ $OS == 'Ubuntu' ]; then
         INSTALL_PACKAGE_CMD="apt -y install"
     
         # Repository update for ansible
-#	apt -y install python-apt
-#        apt -y install software-properties-common
-#        apt-add-repository --yes --update ppa:ansible/ansible
-#        apt update
-#        apt upgrade
+	apt -y install python-apt
+        apt -y install software-properties-common
+        apt-add-repository --yes --update ppa:ansible/ansible
+        apt update
+        apt upgrade
     fi
 fi
 

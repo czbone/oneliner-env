2330a8a72eea337aaa29321b601de4e29b015d97
naoki hirata <naoki@magic3.org>
2019-08-27 16:17:40 +0900

    Ubuntu用修正。

--- script/build_lemp_ffmpeg.sh
+++ script/build_lemp_ffmpeg.sh
@@ -84,14 +84,17 @@ if [ $OS == 'CentOS' ]; then
     yum -y install epel-release
 elif [ $OS == 'Ubuntu' ]; then
     if ! type -P ansible >/dev/null ; then
-        INSTALL_PACKAGE_CMD="apt -y install"
+        #INSTALL_PACKAGE_CMD="apt -y install"
     
         # Repository update for ansible
-	apt -y install python-apt
-        apt -y install software-properties-common
-        apt-add-repository --yes --update ppa:ansible/ansible
-        apt update
-        apt upgrade
+	#apt -y install python-apt
+        #apt -y install software-properties-common
+        #apt-add-repository --yes --update ppa:ansible/ansible
+        #apt update
+        #apt upgrade
+        
+	INSTALL_PACKAGE_CMD="pip3 install"
+        apt -y install python3-pip
     fi
 fi
 

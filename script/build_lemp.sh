4c94ead6c35381544e032d463c9925343baba590
naoki hirata <naoki@magic3.org>
2022-02-06 09:55:56 +0900

    起動スクリプト更新。

--- script/build_lemp.sh
+++ script/build_lemp.sh
@@ -85,7 +85,7 @@ if [ $OS == 'CentOS' ]; then
     
     # Repository update for latest ansible
     #yum install -y epel-release
-    yum install -y python39
+    #yum install -y python39
     #python3 -m pip install --user ansible
     alternatives --set python /usr/bin/python3
     pip3 install --upgrade pip
